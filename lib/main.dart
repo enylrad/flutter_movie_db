c1a5adcb0196135f7f45814e7b910335bf425192
David Perez Salort <enylrad@gmail.com>
2019-09-23 11:31:34 +0200

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_movie_db/src/screens/home.dart';
+
+void main() {
+  runApp(MaterialApp(
+      home: SafeArea(
+    child: Home(),
+  )));
+}
