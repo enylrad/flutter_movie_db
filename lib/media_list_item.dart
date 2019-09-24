8a7a2521e4225003e5d7864aef450c3b81a45c1b
David Perez Salort <enylrad@gmail.com>
2019-09-24 10:15:32 +0200

    Add corners in film items

--- lib/media_list_item.dart
+++ lib/media_list_item.dart
@@ -10,86 +10,88 @@ class MediaListItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      child: Stack(
-        children: <Widget>[
-          getImageBackground(),
-          Positioned(
-            left: 0.0,
-            right: 0.0,
-            bottom: 0.0,
-            child: Container(
-              decoration: BoxDecoration(
-                color: Colors.grey[900].withOpacity(0.5),
-              ),
-              constraints: BoxConstraints.expand(height: 55.0),
-            ),
-          ),
-          Positioned(
-            left: 10.0,
-            bottom: 10.0,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
+    return Container(
+        margin: EdgeInsets.all(5.0),
+        child: ClipRRect(
+            borderRadius: new BorderRadius.circular(8.0),
+            child: Stack(
               children: <Widget>[
-                Container(
-                  width: 250.0,
-                  child: Text(
-                    media.title,
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      color: Colors.white,
+                getImageBackground(),
+                Positioned(
+                  left: 0.0,
+                  right: 0.0,
+                  bottom: 0.0,
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Colors.grey[900].withOpacity(0.5),
                     ),
-                    maxLines: 1,
-                    overflow: TextOverflow.ellipsis,
+                    constraints: BoxConstraints.expand(height: 55.0),
                   ),
                 ),
-                Container(
-                  width: 250.0,
-                  padding: EdgeInsets.only(top: 4.0),
-                  child: Text(
-                    media.getGenres(),
-                    style: TextStyle(color: Colors.white),
-                    maxLines: 1,
-                    overflow: TextOverflow.ellipsis,
+                Positioned(
+                  left: 10.0,
+                  bottom: 10.0,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Container(
+                        width: 250.0,
+                        child: Text(
+                          media.title,
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white,
+                          ),
+                          maxLines: 1,
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                      ),
+                      Container(
+                        width: 250.0,
+                        padding: EdgeInsets.only(top: 4.0),
+                        child: Text(
+                          media.getGenres(),
+                          style: TextStyle(color: Colors.white),
+                          maxLines: 1,
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                      )
+                    ],
                   ),
-                )
-              ],
-            ),
-          ),
-          Positioned(
-            right: 10.0,
-            bottom: 10.0,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.end,
-              children: <Widget>[
-                Row(
-                  children: <Widget>[
-                    Text(media.voteAverage.toString()),
-                    Container(width: 4.0),
-                    Icon(
-                      Icons.star,
-                      color: Colors.white,
-                      size: 16.0,
-                    )
-                  ],
                 ),
-                Row(
-                  children: <Widget>[
-                    Text(media.getRealeaseYear().toString()),
-                    Container(width: 4.0),
-                    Icon(
-                      Icons.date_range,
-                      color: Colors.white,
-                      size: 16.0,
-                    )
-                  ],
+                Positioned(
+                  right: 10.0,
+                  bottom: 10.0,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: <Widget>[
+                      Row(
+                        children: <Widget>[
+                          Text(media.voteAverage.toString()),
+                          Container(width: 4.0),
+                          Icon(
+                            Icons.star,
+                            color: Colors.white,
+                            size: 16.0,
+                          )
+                        ],
+                      ),
+                      Row(
+                        children: <Widget>[
+                          Text(media.getRealeaseYear().toString()),
+                          Container(width: 4.0),
+                          Icon(
+                            Icons.date_range,
+                            color: Colors.white,
+                            size: 16.0,
+                          )
+                        ],
+                      ),
+                    ],
+                  ),
                 ),
               ],
-            ),
-          ),
-        ],
-      ),
-    );
+            )));
   }
 
   FadeInImage getImageBackground() {
