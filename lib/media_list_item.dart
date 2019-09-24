fa50f105fd5339ae59e46412b2243fa261107d6b
David Perez Salort <enylrad@gmail.com>
2019-09-24 10:17:47 +0200

    Refactor widgets item film

--- lib/media_list_item.dart
+++ lib/media_list_item.dart
@@ -17,81 +17,93 @@ class MediaListItem extends StatelessWidget {
             child: Stack(
               children: <Widget>[
                 getImageBackground(),
-                Positioned(
-                  left: 0.0,
-                  right: 0.0,
-                  bottom: 0.0,
-                  child: Container(
-                    decoration: BoxDecoration(
-                      color: Colors.grey[900].withOpacity(0.5),
-                    ),
-                    constraints: BoxConstraints.expand(height: 55.0),
-                  ),
-                ),
-                Positioned(
-                  left: 10.0,
-                  bottom: 10.0,
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Container(
-                        width: 250.0,
-                        child: Text(
-                          media.title,
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            color: Colors.white,
-                          ),
-                          maxLines: 1,
-                          overflow: TextOverflow.ellipsis,
-                        ),
-                      ),
-                      Container(
-                        width: 250.0,
-                        padding: EdgeInsets.only(top: 4.0),
-                        child: Text(
-                          media.getGenres(),
-                          style: TextStyle(color: Colors.white),
-                          maxLines: 1,
-                          overflow: TextOverflow.ellipsis,
-                        ),
-                      )
-                    ],
-                  ),
-                ),
-                Positioned(
-                  right: 10.0,
-                  bottom: 10.0,
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.end,
-                    children: <Widget>[
-                      Row(
-                        children: <Widget>[
-                          Text(media.voteAverage.toString()),
-                          Container(width: 4.0),
-                          Icon(
-                            Icons.star,
-                            color: Colors.white,
-                            size: 16.0,
-                          )
-                        ],
-                      ),
-                      Row(
-                        children: <Widget>[
-                          Text(media.getRealeaseYear().toString()),
-                          Container(width: 4.0),
-                          Icon(
-                            Icons.date_range,
-                            color: Colors.white,
-                            size: 16.0,
-                          )
-                        ],
-                      ),
-                    ],
-                  ),
-                ),
+                getBackgroundDetails(),
+                getTitleAndGenre(),
+                getRatingAndDateRelease(),
               ],
             )));
+  }
+
+  Positioned getBackgroundDetails() {
+    return Positioned(
+      left: 0.0,
+      right: 0.0,
+      bottom: 0.0,
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.grey[900].withOpacity(0.5),
+        ),
+        constraints: BoxConstraints.expand(height: 55.0),
+      ),
+    );
+  }
+
+  Positioned getRatingAndDateRelease() {
+    return Positioned(
+      right: 10.0,
+      bottom: 10.0,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: <Widget>[
+          Row(
+            children: <Widget>[
+              Text(media.voteAverage.toString()),
+              Container(width: 4.0),
+              Icon(
+                Icons.star,
+                color: Colors.white,
+                size: 16.0,
+              )
+            ],
+          ),
+          Row(
+            children: <Widget>[
+              Text(media.getRealeaseYear().toString()),
+              Container(width: 4.0),
+              Icon(
+                Icons.date_range,
+                color: Colors.white,
+                size: 16.0,
+              )
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  Positioned getTitleAndGenre() {
+    return Positioned(
+      left: 10.0,
+      bottom: 10.0,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Container(
+            width: 250.0,
+            child: Text(
+              media.title,
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+              ),
+              maxLines: 1,
+              overflow: TextOverflow.ellipsis,
+            ),
+          ),
+          Container(
+            width: 250.0,
+            padding: EdgeInsets.only(top: 4.0),
+            child: Text(
+              media.getGenres(),
+              style: TextStyle(color: Colors.white),
+              maxLines: 1,
+              overflow: TextOverflow.ellipsis,
+            ),
+          )
+        ],
+      ),
+    );
   }
 
   FadeInImage getImageBackground() {
